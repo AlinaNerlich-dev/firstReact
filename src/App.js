eb5ff122d9f66a0bb59e1a4fc27ba6dfb6c0b7a7
Alina Nerlich <alina.nerlich@gmx.de>
2021-03-26 18:39:47 +0100

    Zweiter Versuch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 
 import './App.css';
+import React from 'react';
 import moviesDb from './moviesDb';
 import Movies from './components/Movies'
 
-function App() {
+class App extends React.Component {
+
+  state = {
+    movieList: moviesDb
+  }
+
+  //Funktionen zum 
+  render(){
   return (
-    <div className="Container">
-      {moviesDb.map((movie, index) => 
-      (<Movies key={index} movie={movie}/>))}
+    <div>
+      <select onChange={e => this.oderMovies(e)} name="orderMovies" id="orderMovies">
+        <option value="Decending">Sorty By Year Decending</option>
+        <option value="Ascending">Sorty By Year Ascending</option>
+      </select>
       
+      <div className="Container">
+        {moviesDb.map((movie, index) => 
+        (<Movies key={index} movie={movie}/>))}
+        
+      </div>
     </div>
   );
+}
 }
 
 export default App;
